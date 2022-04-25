6bdde0fb6c085025abe57ebfa8418a92618fb13d
PeterP1212 <74354368+PeterPerry22@users.noreply.github.com>
2022-04-25 09:24:59 -0400

    commit message

---
+++ src/components/News.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import './News.css'
+
+const News = () => {
+    return (
+        <div className="container">
+            {/* <h1 className="text-center" style={{paddingTop: "30%"}}>
+                News
+            </h1> */}
+            <div class="site-wrap">
+  <section data-scroll>
+    <h1 data-splitting>Equity in the “Data Religion” Era</h1>
+    
+    <p>Oftentimes, we, the general public, tend to mistake sophistication with accuracy and repetition with truth. Unfortunately, this practice becomes a huge fallacy in the human’s modern imperative of uncovering the realities behind the systems we engage with on a daily basis. As Bill Foster, the leader of a webinar on Equitable Algorithms, points out: “AI can inherently be biased.” In fact, credit markets often reflect our nation’s history of discrimination. According to Melissa Koida, 20% of US adults lack a sufficient credit history to be scored and another 30% struggle to access affordable credit because their scores are non-prime. Additionally, nearly 30% of African-Americans and Hispanics cannot be scored under traditional means, compared to 16% of whites and Asians. Lisa Rice further scrutinizes the lending and housing systems in place in America by unveiling that 40% of black adults in Detroit, Michigan are credit invisible. All of these statistics prove that current methodologies in place within financial institutions are unequitable and further discriminatory practices. As Bill Foster states, “No one should be denied the opportunity to own a home, a pillar of the American dream, because of a non-human automated and awful unlawfully discriminatory decision.” However, making AI fair is much easier said than done. To truly initiate change in the procedures of these monetary establishments, Lisa Rice suggests “ Integrat[ing] review of racial and other forms of bias into every phase of the algorithm’s life cycle, including data selection, development, deployment, and monitoring” and ensuring “AI stakeholders, including regulators, scientists, engineers, and more [are] trained on fair housing and fair lending issues.” None of the panelists disagree with the notion that AI can greatly exceed our goals for equality, but all agree that achieving them will take a great level of cooperation between, our government, corporate enterprises, and the public. Stephen Hayes introduces the distinction between disparate treatment disparate impact, which are terms for the intentional and unintentional challenges caused for minorities due to unethical systems utilized by banking institutions at assessing creditworthiness and other metrics that are essential to living an enriching life. Hayes suggests that substituting “variables in the models with the goal of identifying variations of models that maintain performance but that have less disparate impact” is beneficial for companies and consumers. Additionally, he states that more holistic efforts, such as “fair lending training for modelers, ensuring teams have diverse backgrounds, and reviewing policies within which models operate” are also helpful courses of action. He believes that companies do not want to further discrimination, but finds it difficult to locate machine learning models that can detect accurately. Kareem Saleh offers further insight into the aforementioned practice, suggesting that although this method of “taking credit scores out of an algorithm, rerunning it, and evaluating the differences in outcomes for protected groups” may result in fairer outcomes, it is often less profitable for the company. These businesses have no guidance as to how they should handle the trade-off between profitability and equitability. Additionally, “lenders fear that the very act of trying to find a fairer better means of underwriting or pricing loans could be used against them as evidence they knew their algorithms were biased, to begin with.” Surprisingly, there are models that can disentangle a credit score’s predictive power from its disparity driving effects, which results in an increased approval rate for protected groups of about 10-30% without increasing risk for banks. He also mentions that policymakers can further assist by establishing goals or practices for balancing profitability with equitability. Overall, everybody agrees that AI models will have a large impact on the financial institutions within America, and everybody sees their extraordinary potential for good, but the challenge is ensuring that at every step of the process of advancing machines fair outcomes are prioritized.</p>
+  </section>
+  <section data-scroll>
+    <h1 data-splitting>Articles fading in</h1>
+    <p>Consectetur adipisicing elit. Tempore tempora rerum, est autem cupiditate, corporis a qui libero ipsum delectus quidem dolor
+      at nulla, adipisci veniam in reiciendis aut asperiores omnis blanditiis quod quas laborum nam! Fuga ad tempora in aspernatur
+      pariaturlores sunt esse magni, ut, dignissimos.</p>
+    <p>Lorem ipsum cupiditate, corporis a qui libero ipsum delectus quidem dolor at nulla, adipisci veniam in reiciendis aut asperiores
+      omnis blanditiis quod quas laborum nam! Fuga ad tempora in aspernatur pariatur fugit quibusdam dolores sunt esse magni,
+      ut, dignissimos. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore tempora rerum, est autem cupiditate, corporis a qui libero
+      ipsum delectus quidem dolor at nulla, adipisci veniam in reiciendis aut asperiores omnis blanditiis quod quas laborum nam!
+      Fuga ad tempora in aspernatur pariatur fugit quibusdam dolores sunt esse magni, ut, dignissimos.</p>
+    <p>Lorem ipsum cupiditate, corporis a qui libero ipsum delectus quidem dolor at nulla, adipisci veniam in reiciendis aut asperiores
+      omnis blanditiis quod quas laborum nam! Fuga ad tempora in aspernatur pariatur fugit quibusdam dolores.</p>
+  </section>
+
+  <section  data-scroll>
+    <h1 data-splitting>Character splitting</h1>
+    <p>Consectetur adipisicing elit. Tempore tempora rerum, est autem cupiditate, corporis a qui libero ipsum delectus quidem dolor
+      at nulla, adipisci veniam in reiciendis aut asperiores omnis blanditiis quod quas laborum nam! Fuga ad tempora in aspernatur
+      pariaturlores sunt esse magni, ut, dignissimos.</p>
+    <p>Lorem ipsum tempora in cupiditate, corporis a qui libero ipsum delectus quidem dolor at nulla, adipisci veniam in reiciendis aut asperiores
+      omnis blanditiis quod quas laborum nam! Fuga ad tempora in aspernatur pariatur fugit quibusdam dolores sunt esse magni,
+      ut, dignissimos. Adipisci veniam in reiciendis aut asperiores omnis blanditiis quod quas laborum nam! Fuga ad tempora in aspernatur pariaturlores
+      sunt esse magni. Tempore tempora rerum, est autem cupiditate, corporis a qui libero.</p>
+  </section>
+
+  <section  data-scroll>
+    <h1 data-splitting>Colors changing</h1>
+    <p>Consectetur adipisicing elit. Tempore tempora rerum, est autem cupiditate, corporis a qui libero ipsum delectus quidem
+      dolor at nulla, adipisci veniam in reiciendis aut asperiores omnis blanditiis quod quas laborum nam! Fuga ad tempora
+      in aspernatur pariaturlores sunt esse magni, ut, dignissimos.</p>
+    <p>Lorem ipsum cupiditate, corporis a qui libero ipsum delectus quidem dolor at nulla, adipisci veniam in reiciendis aut asperiores
+      omnis blanditiis quod quas laborum nam! Fuga ad tempora in aspernatur pariatur fugit quibusdam dolores sunt esse magni,
+      ut, dignissimos.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates, deserunt facilis et iste corrupti omnis tenetur est.
+      Iste ut est dicta dolor itaque adipisci, dolorum minima, veritatis earum provident error molestias. Ratione magni illo
+      sint vel velit ut excepturi consectetur suscipit, earum modi accusamus voluptatem nostrum, praesentium numquam, reiciendis
+      voluptas sit id quisquam. Consequatur in quis reprehenderit modi perspiciatis necessitatibus saepe, quidem, suscipit
+      iure natus dignissimos ipsam, eligendi deleniti accusantium, rerum quibusdam fugit perferendis et optio recusandae sed
+      ratione. Culpa, dolorum reprehenderit harum ab voluptas fuga, nisi eligendi natus maiores illum quas quos et aperiam
+      aut doloremque optio maxime fugiat doloribus. Eum dolorum expedita quam, nesciunt</p>
+    <p>Adipisicing elit. Tempore tempora rerum.</p>
+  </section>
+
+  <section  data-scroll>
+    <h1 data-splitting>The end</h1>
+    <p>Consectetur adipisicing elit. Tempore tempora rerum, est autem cupiditate, corporis a qui libero ipsum delectus quidem
+      dolor at nulla, adipisci veniam in reiciendis aut asperiores omnis blanditiis quod quas laborum nam! Fuga ad tempora
+      in aspernatur pariaturlores sunt esse magni, ut, dignissimos.</p>
+    <p>Adipisicing elit. Tempore tempora rerum.</p>
+    <p>Lorem ipsum cupiditate, corporis a qui libero ipsum tempora in delectus quidem dolor at nulla, adipisci veniam in reiciendis aut asperiores
+      omnis blanditiis quod quas laborum nam! Fuga ad tempora in aspernatur pariatur fugit quibusdam tempora in dolores sunt esse magni,
+      ut, dignissimos.</p>
+  </section>
+
+</div>
+        </div>
+    )
+}
+export default News;
